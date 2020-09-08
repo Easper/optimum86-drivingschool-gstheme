c35ef2d20c9fc2f6c87c5d0bd7ff0277108df4cf
Easper <xenus_86@yahoo.com>
2020-09-08 12:57:23 +0500

    delete jquery slide effect script for dropdown menu

--- footer.inc.php
+++ footer.inc.php
@@ -26,31 +26,12 @@
     $(".mobile-menu-button").click(function(){
         $("#main-nav").toggleClass("menu-opened");
     });
-
-    $("#main-nav .open > a").on("click", function() {
-        var current_dropdown = $(this).next("ul");
-        $("#main-nav .open ul ul").not(current_dropdown).slideUp("fast");
-        current_dropdown.slideToggle("fast");
-        event.stopPropagation();
+    $("#main-nav ul .open > a").click(function(event) {
         event.preventDefault();
-        return false;
-    });
-
-    $("#main-nav > ul > .open > a").on("click", function() {
-        var current_dropdown = $(this).next("ul");
-        $("#main-nav > ul > .open > ul").not(current_dropdown).slideUp("fast");
-        return false;
-    });
-
-    $(document).on("click", function(event) {
-        var $trigger = $("#main-nav .open");
-        if ($trigger !== event.target && !$trigger.has(event.target).length) {
-            $("#main-nav .open ul").slideUp("fast");
-        }
     });
 </script>
 <!-- add horiz table scroll for mobile devices -->
-<script>$( "#main-content .content table" ).wrap( "<div class='table-container'></div>" );</script>
+<script>$("#main-content .content table").wrap("<div class='table-container'></div>");</script>
 
 <?php get_footer(); ?>
 </body>
